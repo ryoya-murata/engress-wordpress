cc0e4da8e592406b5f455c6c3c0d2141ccb85532
ryoya-murata <ryoya.workng@gmail.com>
2021-02-21 14:52:35 +0900

    サイドバーをテンプレート化

---
+++ sidebar.php
@@ -0,0 +1,45 @@
+<aside class="blog-single-contents__aside-wrapper blog-single-aside">
+              <div class="blog-related blog-single-aside__related">
+                <h2 class="blog-single-aside__title">関連記事</h2><!-- /.blog-single-aside__title -->
+                <div class="blog-related-items">
+                  <a href="#" class="blog-related-items__link">
+                    <div class="blog-related-item blog-related-items__item">
+                      <div class="blog-related-item__img-wrapper">
+                        <img src="./images/front/sample01.png" alt="" class="blog-related-item__img">
+                      </div><!-- /.blog-related-item__img-wrapper -->
+                      <div class="blog-related-item__title-wrapper">
+                        <h3 class="blog-related-item__title">ブログタイトルテキストテキスト</h3><!-- /.blog-related-item__title -->
+                      </div><!-- /.blog-related-item__title-wrapper -->
+                    </div><!-- /.blog-related-item blog-related-items__item -->
+                  </a><!-- /.blog-related-items__link -->
+                 <a href="#" class="blog-related-items__link">
+                  <div class="blog-related-item blog-related-items__item">
+                    <div class="blog-related-item__img-wrapper">
+                      <img src="./images/front/sample01.png" alt="" class="blog-related-item__img">
+                    </div><!-- /.blog-related-item__img-wrapper -->
+                    <div class="blog-related-item__title-wrapper">
+                      <h3 class="blog-related-item__title">ブログタイトルテキストテキストテキストテキスト</h3><!-- /.blog-related-item__title -->
+                    </div><!-- /.blog-related-item__title-wrapper -->
+                  </div><!-- /.blog-related-item blog-related-items__item -->
+                 </a><!-- /.blog-related-item__link -->
+                  <a href="#" class="blog-related-items__link">
+                    <div class="blog-related-item blog-related-items__item">
+                      <div class="blog-related-item__img-wrapper">
+                        <img src="./images/front/sample01.png" alt="" class="blog-related-item__img">
+                      </div><!-- /.blog-related-item__img-wrapper -->
+                      <div class="blog-related-item__title-wrapper">
+                        <h3 class="blog-related-item__title">ブログタイトルテキストテキストテキストテキストテキストテキスト</h3><!-- /.blog-related-item__title -->
+                      </div><!-- /.blog-related-item__title-wrapper -->
+                    </div><!-- /.blog-related-item blog-related-items__item -->
+                  </a><!-- /.blog-related-item__link -->
+                </div><!-- /.blog-related-items -->
+              </div><!-- /.blog-related -->
+              <div class="blog-category">
+                <h2 class="blog-single-aside__title">カテゴリー</h2><!-- /.blog-single-aside__title -->
+                <ul class="blog-category-items">
+                  <li class="blog-catogory-items__item blog-category-item"><a href="#" class="blog-category-item__link ">・カテゴリー1</a><!-- /.blog-category-item  --></li><!-- /.blog-category-item-wrapper -->
+                  <li class="blog-catogory-items__item blog-category-item"><a href="#" class="blog-category-item__link ">・カテゴリー1</a><!-- /.blog-category-item  --></li><!-- /.blog-category-item-wrapper -->
+                  <li class="blog-catogory-items__item blog-category-item"><a href="#" class="blog-category-item__link ">・カテゴリー1</a><!-- /.blog-category-item  --></li><!-- /.blog-category-item-wrapper -->
+                </ul><!-- /.blog-category-items -->
+              </div><!-- /.blog-category -->
+            </aside><!-- /.blog-single-contents__aside-wrapper blog-single-aside -->
--- single.php
+++ single.php
@@ -138,51 +138,7 @@
               </div><!-- /.blog-pickup -->
             </main><!-- /.blog-single-contents__item-wrapper -->
 
-            <aside class="blog-single-contents__aside-wrapper blog-single-aside">
-              <div class="blog-related blog-single-aside__related">
-                <h2 class="blog-single-aside__title">関連記事</h2><!-- /.blog-single-aside__title -->
-                <div class="blog-related-items">
-                  <a href="#" class="blog-related-items__link">
-                    <div class="blog-related-item blog-related-items__item">
-                      <div class="blog-related-item__img-wrapper">
-                        <img src="./images/front/sample01.png" alt="" class="blog-related-item__img">
-                      </div><!-- /.blog-related-item__img-wrapper -->
-                      <div class="blog-related-item__title-wrapper">
-                        <h3 class="blog-related-item__title">ブログタイトルテキストテキスト</h3><!-- /.blog-related-item__title -->
-                      </div><!-- /.blog-related-item__title-wrapper -->
-                    </div><!-- /.blog-related-item blog-related-items__item -->
-                  </a><!-- /.blog-related-items__link -->
-                 <a href="#" class="blog-related-items__link">
-                  <div class="blog-related-item blog-related-items__item">
-                    <div class="blog-related-item__img-wrapper">
-                      <img src="./images/front/sample01.png" alt="" class="blog-related-item__img">
-                    </div><!-- /.blog-related-item__img-wrapper -->
-                    <div class="blog-related-item__title-wrapper">
-                      <h3 class="blog-related-item__title">ブログタイトルテキストテキストテキストテキスト</h3><!-- /.blog-related-item__title -->
-                    </div><!-- /.blog-related-item__title-wrapper -->
-                  </div><!-- /.blog-related-item blog-related-items__item -->
-                 </a><!-- /.blog-related-item__link -->
-                  <a href="#" class="blog-related-items__link">
-                    <div class="blog-related-item blog-related-items__item">
-                      <div class="blog-related-item__img-wrapper">
-                        <img src="./images/front/sample01.png" alt="" class="blog-related-item__img">
-                      </div><!-- /.blog-related-item__img-wrapper -->
-                      <div class="blog-related-item__title-wrapper">
-                        <h3 class="blog-related-item__title">ブログタイトルテキストテキストテキストテキストテキストテキスト</h3><!-- /.blog-related-item__title -->
-                      </div><!-- /.blog-related-item__title-wrapper -->
-                    </div><!-- /.blog-related-item blog-related-items__item -->
-                  </a><!-- /.blog-related-item__link -->
-                </div><!-- /.blog-related-items -->
-              </div><!-- /.blog-related -->
-              <div class="blog-category">
-                <h2 class="blog-single-aside__title">カテゴリー</h2><!-- /.blog-single-aside__title -->
-                <ul class="blog-category-items">
-                  <li class="blog-catogory-items__item blog-category-item"><a href="#" class="blog-category-item__link ">・カテゴリー1</a><!-- /.blog-category-item  --></li><!-- /.blog-category-item-wrapper -->
-                  <li class="blog-catogory-items__item blog-category-item"><a href="#" class="blog-category-item__link ">・カテゴリー1</a><!-- /.blog-category-item  --></li><!-- /.blog-category-item-wrapper -->
-                  <li class="blog-catogory-items__item blog-category-item"><a href="#" class="blog-category-item__link ">・カテゴリー1</a><!-- /.blog-category-item  --></li><!-- /.blog-category-item-wrapper -->
-                </ul><!-- /.blog-category-items -->
-              </div><!-- /.blog-category -->
-            </aside><!-- /.blog-single-contents__aside-wrapper blog-single-aside -->
+            <?php get_sidebar();  ?>
         </div><!-- /.blog-single-contents__inner -->
       </div><!-- /.inner inner--middle -->
     </section><!-- /.section page-section blog-single-contents -->
