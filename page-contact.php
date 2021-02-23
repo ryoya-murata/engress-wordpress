3792490950e5e7fbbe4e7b5d2c4273ef79e10128
ryoya-murata <ryoya.workng@gmail.com>
2021-02-23 15:08:37 +0900

    コンタクトフォームをコーディング

--- page-contact.php
+++ page-contact.php
@@ -13,83 +13,9 @@
 
     <section class="section page-section contact">
       <div class="inner inner--middle">
-        <form action="" class="form contact__form">
-          <p class="form__desc">弊社に興味を持って頂きありがとうございます。商談やサービスのご利用、資料請求について下記のフォームよりお問い合わせください。</p><!-- /.form__desc -->
-          <div class="form__items">
-            <div class="form__item form-item">
-              <div class="form-item__label-wrapper">
-                  <label for="name" class="form-item__label">会社名</label><!-- /.form-item__label -->
-              </div><!-- /.form-item__label-wrapper -->
-              <div class="form-item__input-wrapper">
-                  <input id="name" name="" type="text" class="form-item__input" placeholder="Engress">
-              </div><!-- /.form-item__input-wrapper -->
-          </div><!-- /.form__item form-item -->
-          <div class="form__item form-item">
-            <div class="form-item__label-wrapper">
-                <label for="name" class="form-item__label">氏名</label><!-- /.form-item__label -->
-            </div><!-- /.form-item__label-wrapper -->
-            <div class="form-item__input-wrapper">
-                <input id="name" name="" type="text" class="form-item__input" placeholder="田中　太郎">
-            </div><!-- /.form-item__input-wrapper -->
-          </div>
-            <div class="form__item form-item">
-              <div class="form-item__label-wrapper">
-                  <label for="name" class="form-item__label">メールアドレス</label><!-- /.form-item__label -->
-              </div><!-- /.form-item__label-wrapper -->
-              <div class="form-item__input-wrapper">
-                  <input id="name" name="" type="email" class="form-item__input" placeholder="example@example.com">
-              </div><!-- /.form-item__input-wrapper -->
-            </div><!-- /.form__item form-item -->
-            <div class="form__item form-item">
-              <div class="form-item__label-wrapper">
-                  <label for="name" class="form-item__label">電話番号</label><!-- /.form-item__label -->
-              </div><!-- /.form-item__label-wrapper -->
-              <div class="form-item__input-wrapper">
-                  <input id="name" name="" type="tel" class="form-item__input" placeholder="01-2345-6789">
-              </div><!-- /.form-item__input-wrapper -->
-            </div><!-- /.form__item form-item -->
-            <div class="form__item form-item">
-              <div class="form-item__label-wrapper">
-                  <label for="request" class="form-item__label">お問い合わせの種類を選択してください<span class="form-item__label-caution">（資料請求の方は資料請求を選択ください）</span><!-- /.form-item__label-caution --></label><!-- /.form-item__label -->
-              </div><!-- /.form-item__label-wrapper -->
-              <div class="form-item__input-wrapper">
-                <div class="form-item__radio-container">
-                  <label class="form-item__radio-wrapper"><input name="content" type="radio" class="form-item__radio" value="男性" checked><span class="form-item__radio-label">商談のご相談</span><!-- /.form__radio-label --></label><!-- /.form__radio-wrapper -->
-                </div><!-- /.form-item__radio-container -->
-                <div class="form-item__radio-container">
-                  <label class="form-item__radio-wrapper"><input name="content" type="radio" class="form-item__radio" value="男性" checked><span class="form-item__radio-label">サービスに関するお問い合わせ</span><!-- /.form__radio-label --></label><!-- /.form__radio-wrapper -->
-                </div><!-- /.form-item__radio-container -->
-                <div class="form-item__radio-container">
-                  <label class="form-item__radio-wrapper"><input name="content" type="radio" class="form-item__radio" value="男性" checked><span class="form-item__radio-label">資料請求</span><!-- /.form__radio-label --></label><!-- /.form__radio-wrapper -->
-                </div><!-- /.form-item__radio-container -->
-                <div class="form-item__radio-container">
-                  <label class="form-item__radio-wrapper"><input name="content" type="radio" class="form-item__radio" value="男性" checked><span class="form-item__radio-label">その他</span><!-- /.form__radio-label --></label><!-- /.form__radio-wrapper -->
-                </div><!-- /.form-item__radio-container -->
-              </div><!-- /.form-item__input-wrapper -->
-            </div><!-- /.form__item form-item -->
-            <div class="form__item form-item">
-              <div class="form-item__label-wrapper">
-                  <label for="name" class="form-item__label">お問い合わせ内容</label><!-- /.form-item__label -->
-              </div><!-- /.form-item__label-wrapper -->
-              <div class="form-item__input-wrapper">
-                <textarea name="" id="message"  class="form-item__textarea" placeholder="入力してください"></textarea><!-- /#.form-item__textarea -->
-              </div><!-- /.form-item__input-wrapper -->
-            </div><!-- /.form__item form-item -->
-            <div class="form__privacy-contents">
-              <p class="form__privacy-text">
-                <a href="#" class="form__privacy-link">プライバシーポリシー</a><!-- /.form__privacy-link -->
-                に同意の上、送信ください。
-              </p><!-- /.form__privacy-text -->
-              <div class="form__agree-wrapper">
-                <input id="agree" type="checkbox" class="form__agree-input" name="" value="プライバシーポリシーに同意する" >
-                <label for="agree" class="form__agree-label"><span class="form__agree-caution">プライバシーポリシーに同意する</span></label><!-- /.form__agree-label -->
-            </div><!-- /.form__agree-wrapper -->
-            </div><!-- /.form_privacy-contents -->
-            <div class="button-wrapper button-wrapper--center">
-               <input type="submit" class="button form__submit-button button--color_primary button--shape_square" value="送信する">
-             </div><!-- /.button-wrapper button-wrapper--center -->
-          </div><!-- /.form__items -->
-        </form><!-- /.form contact__form -->
+        <div class="contact__form">
+        <?php the_content(); ?>
+        </div><!-- /.contact__form -->
       </div><!-- /.inner inner--middle -->
     </section><!-- /.section page-section contact -->
 
